127e2c508b5861eef4f04f10efd86e57338b97ce
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-01 08:31:58 +0500

    add card title, img, text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import Button from 'react-bootstrap/Button';
-import {Button, Alert, Breadcrumb} from 'react-bootstrap';
+import {Button, Alert, Breadcrumb, Card} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className='App-header'>
+        <Card style={{ color: "#333" }}>
+          <Card.Img />
+          <Card.Body>
+            <Card.Title>Card Example</Card.Title>
+            <Card.Text>
+              This is an example of cards
+            </Card.Text>
+            <Button variant='primary'>Read More</Button>
+          </Card.Body>
+        </Card>
         <Breadcrumb>
           <Breadcrumb.Item>crumb.Item 1</Breadcrumb.Item>
           <Breadcrumb.Item>crumb.Item 2</Breadcrumb.Item>
