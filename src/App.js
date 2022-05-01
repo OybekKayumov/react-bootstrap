41688b5e3678bda3f104dbe3a34300d117eb3669
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-01 08:43:17 +0500

    add form label, control, text

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import Button from 'react-bootstrap/Button';
-import {Button, Alert, Breadcrumb, Card} from 'react-bootstrap';
+import {Button, Alert, Breadcrumb, Card, Form} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className='App-header'>
+      <Form>
+        <Form.Group>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type="email" placeholder="example@email.com" />
+          <Form.Text className="text-muted">
+            Enter valid email address
+          </Form.Text>
+        </Form.Group>
+
+
+      </Form>
         <Card className="mb-3" style={{ color: "#333", marginBottom: 20 }}>
-          <Card.Img src="https://picsum.photos/200/100" />
+          <Card.Img src="https://picsum.photos/200/50" />
           <Card.Body>
             <Card.Title>Card Example</Card.Title>
             <Card.Text>
