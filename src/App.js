8d4f60849a55cd8cb3a79b3a1445f06b7420397f
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-01 08:26:22 +0500

    add breadcrumb items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import Button from 'react-bootstrap/Button';
-import {Button, Alert} from 'react-bootstrap';
+import {Button, Alert, Breadcrumb} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className='App-header'>
+        <Breadcrumb>
+          <Breadcrumb.Item>crumb.Item 1</Breadcrumb.Item>
+          <Breadcrumb.Item>crumb.Item 2</Breadcrumb.Item>
+          <Breadcrumb.Item active>crumb.Item 3</Breadcrumb.Item>
+        </Breadcrumb>
         <Alert variant="success">This is a alert</Alert>
         <Button>Test Button</Button>
       </header>      
