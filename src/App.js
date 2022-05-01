fd0d73ab68ec90e23c2d1edfb56bbce9d52fb86d
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-05-01 08:13:37 +0500

    add bootstrap and react-bootstrap

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Test Button</Button>
+      
+    </div>
+  );
+}
+
+export default App;
